6b071068f4f150ae9d32a6d2a76400949d9ffb4a
Asad Ansari <asadansari@users.noreply.github.com>
2024-09-07 22:39:15 -0400

    Update wake_up.sh

--- wake_up.sh
+++ wake_up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/secrets/desktop_mac_address.sh
+source ~/secrets/desktop_mac_addresses.sh
 
 sudo etherwake $DESKTOP_MAC_ADDRESS_WIRED
 sudo wakeonlan $DESKTOP_MAC_ADDRESS_WIRED
